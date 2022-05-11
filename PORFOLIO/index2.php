892daa2b44729400aeacd71fcde1a6a0c4b1da59
adrians_diaz <asanchez@ya-voz.com>
2022-05-11 10:28:24 +0200

    first commit

---
+++ PORFOLIO/index2.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-">
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
+</head>
+<body>
+    <header>
+        <img src="./assets/svg/CarritoOn.svg" alt="Sobre mi" class="nav-logo">
+        <nav>
+            <ul>
+                <li>
+                    CONTACTO
+                </li>
+                <li>
+                    REDES
+                </li>
+                <li>
+                    PRINCIPAL
+                </li>
+                <li>
+                    SALIR
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+</body>
+</html>
